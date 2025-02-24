461ad456e85ca7a72edf5a1fbab67af4b5f9d7f5
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2025-02-23 22:07:06 -0300

    up route login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,35 @@
+
+import { useState } from 'react'
+
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    // Add your login logic here
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
