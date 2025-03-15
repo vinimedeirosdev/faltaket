e75ef9878c40217cdb0467a100673a0744b74733
Vinicius Medeiros <viniciusmedeiros779@gmail.com>
2025-03-15 17:41:07 -0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,22 @@
-import { Avatar, Fab, Icon } from "@mui/material";
+import {
+  Avatar,
+  Divider,
+  Fab,
+  Icon,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import globalState from "../store/globalState";
 import { useNavigate } from "react-router-dom";
-import { Add } from "@mui/icons-material";
+import { Add, Logout, Person } from "@mui/icons-material";
 import avatarImg from "../assets/avatarImg.jpg";
+import { useState } from "react";
 
 function Home() {
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
   const actions = {
     async onClickLogout() {
@@ -18,6 +29,14 @@ function Home() {
       };
 
       navigate("/");
+    },
+
+    handleOpenMenu(event: React.MouseEvent<HTMLElement>) {
+      setAnchorEl(event.currentTarget);
+    },
+
+    handleCloseMenu() {
+      setAnchorEl(null);
     },
   };
 
@@ -43,19 +62,49 @@ function Home() {
           </Icon>
         </Fab>
 
-        <Fab
-          size="medium"
-          sx={{}}
-          onClick={() => {
-            /* sua função de clique aqui */
-          }}
-        >
+        <Fab size="medium" sx={{}} onClick={actions.handleOpenMenu}>
           <Avatar
             sx={{ width: "100%", height: "100%" }}
             src={avatarImg}
           ></Avatar>
         </Fab>
+
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={actions.handleCloseMenu}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem>
+            <Person fontSize="small" sx={{ mr: 1 }} />
+            Perfil
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={actions.onClickLogout}>
+            <Logout fontSize="small" sx={{ mr: 1 }} />
+            Logout
+          </MenuItem>
+        </Menu>
       </div>
+
+      <div
+        style={{
+          marginTop: "16px",
+        }}
+      >
+        <Typography
+          variant="h2"
+          align="center"
+          sx={{
+            fontFamily: "Caveat",
+          }}
+        >
+          Faltaket
+        </Typography>
+      </div>
+
+      <div style={{ marginTop: "16px" }}></div>
     </div>
   );
 }
